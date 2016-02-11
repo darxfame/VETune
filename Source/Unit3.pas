913a942a4d6aa0abb874212ce2d8523f0dd985a9
metra52 <metra52@ya.ru>
2016-02-11 20:59:44 +0300

    Add GNU License

--- Source/Unit3.pas
+++ Source/Unit3.pas
@@ -1,3 +1,20 @@
+{ VETune and VEOnline  - An open source, free editor engine tables unit
+   Copyright (C) 2015 Artem E. Kochegizov. Russia, Moscow
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   contacts:
+              http://secu-3.org
+              email: akochegizov@gmail.com
+}
+
 unit Unit3;
 
 interface
